6e4f43e8eb9a281d2cf1c7edb38fb12d793fc2b4
liokusnata <liokusnata321@gmail.com>
2025-10-29 14:15:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let searchBtn=document.querySelector('#search-btn');
+let searchBar=document.querySelector('.search-bar-container');
+
+let loginForm=document.querySelector('.login-form-container');
+let user=document.querySelector('#login-btn');
+let money=document.querySelector('.moneyOptions');
+let signUpMoney=document.querySelector('#signUpMoney');
+
+let cancelBtn=document.querySelector('#form-close');
+
+let videoBtn=document.querySelector('.controls');
+let videoBackground = document.getElementById("vid-back")
+
+let menu=document.querySelector('#menu-bar');
+let navbar=document.querySelector('.navbar');
+
+
+window.onscroll=function(){
+    searchBtn.classList.remove('fa-times')
+    
+    searchBar.classList.remove('active');
+}
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active');
+})
+searchBtn.addEventListener('click',()=>{
+    searchBtn.classList.toggle('fa-times')
+    searchBar.classList.toggle('active');
+})
+
+user.addEventListener('click',()=>{
+    loginForm.classList.toggle('showLogin');
+})
+money.addEventListener('click',()=>{
+    loginForm.classList.toggle('showLogin');
+})
+signUpMoney.addEventListener('click',()=>{
+    loginForm.classList.toggle('showLogin');
+})
+
+cancelBtn.addEventListener('click',()=>{
+    loginForm.classList.remove('showLogin');
+})
+
+
+
+videoBtn.childNodes.forEach(btn=>{
+    btn.addEventListener('click',(e)=>{
+        document.querySelector('.vid-btn.active').classList.remove('active');
+        e.target.classList.add('active');
+        let src=btn.getAttribute('data-src');
+        if(src==="./clip-1-Indo.mp4"){
+            videoBackground.src = "./img1.png";
+        }else{
+            videoBackground.src = "./img2.png";
+        }
+        document.querySelector('#video-slider').src=src;
+    })
+})
